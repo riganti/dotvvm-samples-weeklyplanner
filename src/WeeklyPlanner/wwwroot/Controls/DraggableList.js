15d5dfbe0ed94cecc18c823a2effba0214859f4d
Tomáš Herceg <tomas.herceg@riganti.cz>
2018-05-25 14:30:41 +0200

    Submodule removed DotVVM referenced from NuGet

--- src/WeeklyPlanner/wwwroot/Controls/DraggableList.js
+++ src/WeeklyPlanner/wwwroot/Controls/DraggableList.js
@@ -1,4 +1,4 @@
-///<reference path="../../../../dotvvm/src/DotVVM.Framework/Resources/Scripts/typings/knockout/knockout.d.ts" />
+///<reference path="../typings/knockout.d.ts" />
 ///<reference path="../typings/jquery.d.ts" />
 class DraggableList {
     constructor($element, binding, allBindings) {
